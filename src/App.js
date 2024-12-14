db9638b25cd14d4563bf5106ec4111012dc80d5a
Ritzrawal <riteshrawal31@gmail.com>
2024-12-14 18:58:20 -0500

    added the new nextjs file in react portfolio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Homepage } from "./screen/index";
 
+("use client");
 import "./App.css";
 
 const App = () => {
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+"use client";
+import React, { useState } from "react";
+
+const App: any = () => {
+  return <div>Hello data </div>;
+};
+
+export default App;
