11885a9691322c6f57c64ecbfb2f0c202c07daf5
Ritzrawal <riteshrawal31@gmail.com>
2025-01-26 15:55:30 -0500

    modified the css by tailwind css

--- src/app/screen/professional-screen.tsx
+++ src/app/screen/professional-screen.tsx
@@ -7,9 +7,9 @@ import ComputerSvgLogo from "../component/computerSvg";
 
 const ProfessionalScreen: React.FC = () => {
   return (
-    <div className='computer-main-container'>
-      <div className='computer-main-rightside-container'>
-        <div className='computer-main-title-logo'>
+    <div className='flex flex-row justify-between items-center h-screen bg-gradient-to-r from-[#859398] via-[#5A6A71] to-[#283048]'>
+      <div className='flex flex-col w-1/2 text-[18px]'>
+        <div className='flex w-1/2 pb-1/4 items-center justify-center'>
           <RSvgLogo />
         </div>
         <div className='text-3xl font-bold text-start pl-12 relative animate-title bg-gradient-to-r from-[#FF7F50] to-[#00FFFF] bg-clip-text text-transparent'>
@@ -18,15 +18,15 @@ const ProfessionalScreen: React.FC = () => {
 
         <div className='flex relative p-12 text-start font-normal text-[#D3D3D3] font-sans text-lg animate-details'>
           Hey there, welcome to my Portfolio! I am Ritesh, a highly motivated
-          and dedicated React Frontend Developer with strong knowledge of React,
-          Redux, React Native, NextJs, Tailwind CSS, JavaScript, CSS, Node, and
-          HTML. Seeking an opportunity to leverage my skills and knowledge to
-          contribute to a dynamic development team while continuously enhancing
-          my expertise.
+          and dedicated React and React-Native Frontend Developer with strong
+          knowledge of React, Redux, React Native, NextJs, Tailwind CSS,
+          JavaScript, CSS, Node, and HTML. Seeking an opportunity to leverage my
+          skills and knowledge to contribute to a dynamic development team while
+          continuously enhancing my expertise.
         </div>
       </div>
 
-      <div className='computer-image'>
+      <div className='px-24 animate-computer w-[45%]'>
         <ComputerSvgLogo />
       </div>
     </div>
